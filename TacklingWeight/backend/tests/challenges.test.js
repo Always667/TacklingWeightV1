4f166cb0fded384cb9d90df89710aad119d5723a
Always667 <tyreekrobinson1327@gmail.com>
2026-03-21 08:13:07 +0530

    Update challenges.test.js

--- TacklingWeight/backend/tests/challenges.test.js
+++ TacklingWeight/backend/tests/challenges.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const app = require('../server');
 const User = require('../models/User');
